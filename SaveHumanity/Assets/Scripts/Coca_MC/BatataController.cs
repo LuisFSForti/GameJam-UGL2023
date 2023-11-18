c77d3d663632dbdfab162221e06754bc35e611c7
LuisFSForti <luisforti@usp.br>
2023-11-17 21:04:59 -0300

    Ataque das batatas
    Era um erro de código mesmo.
    Programação do ataque das batatas do chefão McDonalds.

--- SaveHumanity/Assets/Scripts/Coca_MC/BatataController.cs
+++ SaveHumanity/Assets/Scripts/Coca_MC/BatataController.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static System.Net.WebRequestMethods;
 
 public class BatataController : MonoBehaviour
 {
     private GameObject Player;
-    private float Velocidade = 2f;
+    private float Velocidade = 8f;
+    private bool Preparando = true;
 
     // Start is called before the first frame update
     void Start()
@@ -13,15 +15,29 @@ public class BatataController : MonoBehaviour
         
     }
 
-    public void Ativar(GameObject player, Transform transform)
+    public void Ativar(GameObject player, Transform origem)
     {
         Player = player;
-        this.transform.position = transform.position;
+        this.transform.position = origem.position;
+        this.transform.eulerAngles = new Vector3(0, 0, 90);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.forward * Time.deltaTime * Velocidade;
+        transform.position += transform.right * Velocidade * Time.deltaTime;
+
+        if(Preparando)
+        {
+            if(transform.position.y > 3)
+            {
+                Preparando = false;
+
+                //https://www.youtube.com/watch?v=RNPetUa8_PQ
+                Vector3 vectorToTarget = Player.transform.position - transform.position;
+                float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+                transform.eulerAngles = new Vector3(0, 0, angle);
+            }
+        }
     }
 }
--- SaveHumanity/Assets/Scripts/Coca_MC/McDonaldsBehaviour.cs
+++ SaveHumanity/Assets/Scripts/Coca_MC/McDonaldsBehaviour.cs
@@ -6,58 +6,59 @@ using UnityEngine;
 public class McDonaldsBehaviour : MonoBehaviour
 {
     private float IntervaloMinimo = 3f, IntervaloMaximo = 5f, UltimoAtaque = 0f;
-    private float DuracaoBatata = 5f, IntervaloBatata = 0.5f, UltimoTiro = 0f;
+    private float DuracaoBatata = 2f, IntervaloBatata = 0.25f, UltimoTiro = 0f;
 
+    private int Ataque = 0;
     public GameObject BatataPrefab, NuggetPrefab, HamburguerPrefab, Player;
 
     // Start is called before the first frame update
     void Start()
     {
-        UltimoAtaque = Time.time;
+        UltimoAtaque = Time.time + IntervaloMaximo;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(UltimoAtaque <= Time.time)
+        if(UltimoAtaque <= Time.time && Ataque == 0)
         {
             UltimoAtaque = Time.time;
-            Debug.Log(UltimoAtaque);
-            int teste = Random.Range(1, 5);
-            Debug.Log(teste);
-            switch (teste)
-            {
-                case 1:
-                case 2:
-                    Debug.Log("BBBBBBBBB");
-                    Debug.Break();
-                    AtaqueBatatas();
-                    break;
+            Ataque = Random.Range(1, 5);
+        }
 
-                case 3:
-                    AtaqueNuggets();
-                    break;
+        switch (Ataque)
+        {
+            case 1:
+            case 2:
+                AtaqueBatatas();
 
-                case 4:
-                    AtaqueHamburguer();
-                    break;
+                if (UltimoAtaque + DuracaoBatata < Time.time)
+                {
+                    UltimoAtaque += Random.Range(IntervaloMinimo, IntervaloMaximo);
+                    Ataque = 0;
+                }
 
-                default: break;
-            }
-            UltimoAtaque += Random.Range(IntervaloMinimo, IntervaloMaximo);
+                break;
+
+            case 3:
+                AtaqueNuggets();
+                break;
+
+            case 4:
+                AtaqueHamburguer();
+                break;
+
+            default: break;
         }
     }
 
     void AtaqueBatatas()
     {
-        while(Time.time < UltimoAtaque + DuracaoBatata)
+        if(UltimoTiro + IntervaloBatata < Time.time)
         {
-            if(UltimoTiro + IntervaloBatata < Time.time)
-            {
-                UltimoTiro = Time.time;
-                GameObject instancia = Instantiate(BatataPrefab);
-                instancia.GetComponent<BatataController>().Ativar(Player, transform);
-            }
+            UltimoTiro = Time.time;
+            GameObject instancia = Instantiate(BatataPrefab);
+            instancia.GetComponent<BatataController>().Ativar(Player, transform);
         }
     }
 
